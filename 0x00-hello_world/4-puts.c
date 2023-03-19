7dae596190cb792a37759085768fe7b99c36bd6d
Victor Ola <victorolaonifogo@gmail.com>
2023-03-19 13:43:08 -0700

    Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *We are writting a script that print out without using prinntf
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
