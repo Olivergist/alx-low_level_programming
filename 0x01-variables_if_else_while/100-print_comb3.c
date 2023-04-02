af95bd0ed09d6a4a41f5ca12fdb2961f6dfd0936
Victor Ola <victorolaonifogo@gmail.com>
2023-04-01 17:06:56 -0700

    Done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Description: Print in Two Digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0, j = 1;
+
+	while (i < 9)
+
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		if (i != 8 || j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		j++;
+		if (j > 9)
+		{
+			i++;
+			j = i + 1;
+		}
+	}
+
+	return (0);
+}
