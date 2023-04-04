cf38b2a602f1ddd6eeb4c64a50d009fc5208d6d4
Victor Ola <victorolaonifogo@gmail.com>
2023-04-04 10:42:11 -0700

    Done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,13 +7,14 @@
  * @size: Size of the matrix
  * Return: return
  */
-void print_diagsums(int *a, int size) 
+void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
-	for (i = 0; i < size; i++) 
+
+	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i*size + i);
-		sum2 += *(a + i*size + (size - 1 - i));
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - 1 - i));
 	}
-	printf("%d,%d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
