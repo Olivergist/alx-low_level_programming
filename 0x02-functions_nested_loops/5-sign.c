71ad648b87ccf4442e49d300f59cee828c798191
Olivergist <Victorolaonifogo@gmail.com>
2023-07-03 13:25:01 -0700

    Updates

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - The sign of must be printed
+ * @n: check int
+ * Return: 1 and prints + if n is greater than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
