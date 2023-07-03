d84351fb8b2544f6c5f08c78088d41051333d795
Olivergist <Victorolaonifogo@gmail.com>
2023-07-03 09:12:41 -0700

    Updates

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,10 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 0
-
-section .text
-    global main
-    extern printf
-
+global   main
+	  extern    printf
 main:
-    ; Prepare arguments for printf
-    mov rdi, format
-    mov rsi, hello
-    xor eax, eax  ; Clear the upper 32 bits of rax
-
-    ; Call printf
-    call printf
-
-    ; Exit the program
-    xor eax, eax
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
