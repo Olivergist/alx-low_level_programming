224053d6a9ec67d2e93c07f981123ec35d8d17df
Olivergist <Victorolaonifogo@gmail.com>
2023-07-03 12:47:40 -0700

    Updates

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,22 +5,21 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
 	char letter;
+	int count;
+
+	count = 0;
 
 	while (count < 10)
 	{
 		letter = 'a';
-
 		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
 		}
-
+		_putchar('\n');
 		count++;
 	}
-
-	_putchar('\n');
 }
 
