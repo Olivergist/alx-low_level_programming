d7bd79ad200b522d19c7a6b8224bc39eb2a11518
Olivergist <Victorolaonifogo@gmail.com>
2023-07-04 03:16:22 -0700

    Updates

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int fib1 = 1;
+	unsigned int fib2 = 2;
+
+	printf("%u, %u", fib1, fib2);
+
+	for (int i = 3; i <= 98; i++)
+	{
+		unsigned int fib = fib1 + fib2;
+
+		printf(", %u", fib);
+
+		fib1 = fib2;
+		fib2 = fib;
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
